fc4afb3e3d9aaae70892f3006729aae56ed83d57
Bert BRUYNOOGHE <bert.bruynooghe@alcatel-lucent.com>
2010-11-15 08:27:13 +0100

    create pom.xml

---
+++ src/main/java/org/htmlrendertest/App.java
@@ -0,0 +1,13 @@
+package org.htmlrendertest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
