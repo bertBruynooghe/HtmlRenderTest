a0bcb5b3ae38c0e5926b0ab7dcfdd5831b2e334c
Bert BRUYNOOGHE <bert.bruynooghe@alcatel-lucent.com>
2010-11-23 12:17:56 +0100

    fixed running tests from maven

--- src/test/java/UserExtensionsTest.java
+++ src/test/java/UserExtensionsTest.java
@@ -1,27 +1,37 @@
 import com.thoughtworks.selenium.HttpCommandProcessor;
-import org.junit.After;
-import org.junit.Before;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class UserExtensionsTest {
-    private SeleniumExtension selenium;
+    private static SeleniumExtension selenium;
 
-    @Before
-    public void setUp() {
+    @BeforeClass
+    static public void beforeClass() {
         HttpCommandProcessor processor = new HttpCommandProcessor("beantn0a000189", 4545, "*firefox", "http://localhost");
         selenium = new SeleniumExtension(processor);
         selenium.start();
     }
 
-    @After
-    public void tearDown() {
+
+    @AfterClass
+    static public void tearDown() {
         selenium.stop();
     }
 
     @Test
-    public void runSeleneseTestCases() {
+    public void assertEqualsTest() {
         selenium.processSeleneseTestCase("src/test/selenese/testcase/assertEqualsTest.html");
+    }
+
+    @Test
+    public void runBrowserWidthTest() {
         selenium.processSeleneseTestCase("src/test/selenese/testcase/browserWidthTest.html");
+    }
+
+    @Test
+    public void testVerticalScrollFunctionality() {
         selenium.processSeleneseTestCase("src/test/selenese/testcase/getNumberOfVerticalScrollStepsTest.html");
     }
+
 }
